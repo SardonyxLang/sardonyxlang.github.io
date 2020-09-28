1f61096ef3b97fa00ab60f4ee322dcd6447ccc78
Zavexeon <zavexeon@protonmail.com>
2020-09-28 18:22:26 -0400

    yes

---
+++ docs/index.js
@@ -0,0 +1,11 @@
+const links = {
+    '/': 'Introduction',
+    '/data-types': 'Data Types',
+    '/blocks': 'Blocks',
+    '/stdlib': {
+        name: 'Standard Library (stdlib)',
+        dropdownLinks: {
+            '/stdlib/#println': 'println'
+        }
+    }
+};
