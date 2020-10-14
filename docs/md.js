571b5f6518b98d2204650b8e2da1b5427223a3fb
Coder100 <a@a.com>
2020-10-14 01:37:15 +0000

    made the doc navbar sticky, among others

--- docs/md.js
+++ docs/md.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     const converter = new showdown.Converter();
 
-    let text = converter.makeHtml(document.getElementById('md').innerText)
+    let text = converter.makeHtml(document.getElementById('md').innerText);
 
     document.getElementById('md').innerHTML = text;
 }
---
+++ script.js
@@ -0,0 +1,1 @@
+console.log("Sardonyx 2020");
